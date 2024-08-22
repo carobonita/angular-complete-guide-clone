7c8a4020731ffdb11564a768f3ae621a15abada0
Carolina Bonitatis <carolina@bonitat.is>
2024-08-21 21:22:56 -0400

    display name from dummy data

--- code-snapshots/02-essentials/01-starting-project/src/app/app.component.ts
+++ code-snapshots/02-essentials/01-starting-project/src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from "./user/user.component";
 import { DUMMY_USERS } from './dummy-users';
+import { TaskComponent } from './task/task.component';
+
 
 @Component({
     selector: 'app-root',
@@ -10,13 +12,20 @@ import { DUMMY_USERS } from './dummy-users';
     styleUrl: './app.component.css',
     imports: [
         HeaderComponent,
-        UserComponent
+        UserComponent,
+        TaskComponent
     ]
 })
 export class AppComponent {
     users = DUMMY_USERS;
+    selectedUserId = 'u1';
+
+    get seletedUser() {
+        return this.users.find(user => user.id === this.selectedUserId)!;
+
+    }
 
     onSelectedUser(id: string) {
-        console.log('Selected user id: ' + id);
+        this.selectedUserId = id;
     }
 }
