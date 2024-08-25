cb67dd4bfbb05c1dedd97e8f2fbe6d082391b864
Carolina Bonitatis <carolina@bonitat.is>
2024-08-24 22:54:37 -0400

    add tasks and task components. Add style to those components

--- code-snapshots/02-essentials/01-starting-project/src/app/app.component.ts
+++ code-snapshots/02-essentials/01-starting-project/src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from "./user/user.component";
 import { DUMMY_USERS } from './dummy-users';
-import { TaskComponent } from './task/task.component';
+import { TasksComponent } from './tasks/tasks.component';
 
 
 @Component({
@@ -13,14 +13,14 @@ import { TaskComponent } from './task/task.component';
     imports: [
         HeaderComponent,
         UserComponent,
-        TaskComponent
+        TasksComponent
     ]
 })
 export class AppComponent {
     users = DUMMY_USERS;
-    selectedUserId = 'u1';
+    selectedUserId? = 'string';
 
-    get seletedUser() {
+    get selectedUser() {
         return this.users.find(user => user.id === this.selectedUserId)!;
 
     }
