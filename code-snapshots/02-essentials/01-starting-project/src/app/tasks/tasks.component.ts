4765164d20ed0a10f5f764fe15df477a80988a03
Carolina Bonitatis <carolina@bonitat.is>
2024-08-26 21:27:25 -0400

    add filter method for complete tasks

--- code-snapshots/02-essentials/01-starting-project/src/app/tasks/tasks.component.ts
+++ code-snapshots/02-essentials/01-starting-project/src/app/tasks/tasks.component.ts
@@ -9,6 +9,38 @@ import { TaskComponent } from "./task/task.component";
   styleUrl: './tasks.component.css'
 })
 export class TasksComponent {
-  @Input() name?: string;
+  @Input({required: true}) userId!: string;
+  @Input({required: true}) name?: string;
+  tasks = [
+    {
+      id: 't1',
+      userId: 'u1',
+      title: 'Master Angular',
+      summary: 'Learn Angular all the basic and advance features of Angular & how to apply them',
+      dueDate: '2025-12-31'
+    },
+    {
+      id: 't2',
+      userId: 'u3',
+      title: 'Build first prototype',
+      summary: 'build a first prototype of the online shop website',
+      dueDate: '2024-05-31'
+    },
+    {
+      id: 't3',
+      userId: 'u3',
+      title: 'Prepare issues template',
+      summary: 'Prepare and describe an issue template which wil help with project management',
+      dueDate: '2026-06-15'
+    }
+  ];
+
+  get selectedUserTasks() {
+    return this.tasks.filter(task => task.userId === this.userId);
+  }
   
+  onCompleteTask(id: string) {
+    this.tasks = this.tasks.filter((task) => task.id !== id);
+  }
+
 }
