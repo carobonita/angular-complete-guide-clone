9961ccb0819bfb24f75d0f170db2d003f434e186
Carolina Bonitatis <carolina@bonitat.is>
2024-08-26 21:28:48 -0400

    update page

--- code-snapshots/02-essentials/01-starting-project/src/app/user/user.component.ts
+++ code-snapshots/02-essentials/01-starting-project/src/app/user/user.component.ts
@@ -1,11 +1,6 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 
-
-interface User {
-  id: string;
-  avatar: string;
-  name: string;
-}
+import { User } from './user.model';
 
 @Component({
   selector: 'app-user',
@@ -15,7 +10,8 @@ interface User {
 })
 export class UserComponent {
   @Input({required: true}) user!: User;
-  @Output() select = new EventEmitter();
+  @Input({required: true}) selected!: boolean
+  @Output() select = new EventEmitter<string>();
 
 
   get imagePath() { 
