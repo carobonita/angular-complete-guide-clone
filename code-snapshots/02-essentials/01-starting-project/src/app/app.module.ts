d3a1c6abc4828e7cbc73dffdf0c705c81a8e61ba
Carolina Bonitatis <carolina@bonitat.is>
2024-09-28 15:20:03 -0400

    update modules for tasks component

--- code-snapshots/02-essentials/01-starting-project/src/app/app.module.ts
+++ code-snapshots/02-essentials/01-starting-project/src/app/app.module.ts
@@ -1,27 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
-import { TasksComponent } from './tasks/tasks.component';
-import { TaskComponent } from './tasks/task/task.component';
-import { NewTaskComponent } from './tasks/new-task/new-task.component';
 import { SharedModule } from './shared/shared.module';
+import { TasksModule } from './tasks/tasks.module';
 
 
 
 @NgModule({
-  declarations: [
+  declarations: [  //declare and register components, directives, and pipes
     AppComponent, 
     HeaderComponent, 
     UserComponent, 
-    TasksComponent, 
-    TaskComponent, 
-    NewTaskComponent ], //declare and register components, directives, and pipes
+   ],
   bootstrap: [AppComponent], //specify the root component
-  imports: [BrowserModule, FormsModule, SharedModule],
+  imports: [BrowserModule, SharedModule, TasksModule],
 
 })
 export class AppModule {}
