229827a25550fd2525259bd3c600eda16b0baaa4
Carolina Bonitatis <carolina@bonitat.is>
2024-11-13 07:41:37 -0500

    add signals in user component

--- attachments/04-essentials-practice/01-starting-project/src/app/user-input/user-input.component.ts
+++ attachments/04-essentials-practice/01-starting-project/src/app/user-input/user-input.component.ts
@@ -1,25 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, output, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import type { InvestmentInput } from '../investment-input.model';
 
 @Component({
   selector: 'app-user-input',
   standalone: true,
   imports: [FormsModule],
   templateUrl: './user-input.component.html',
-  styleUrl: './user-input.component.css'
+  styleUrl: './user-input.component.css',
 })
 export class UserInputComponent {
-  enteredInitialInvestment = '0';
-  enteredAnnualInterest = '0';
-  enteredExpectedReturn = '5';
-  enteredDuration = '10';
+  calculate = output<InvestmentInput>();
+  enteredInitialInvestment = signal('0');
+  enteredAnnualInterest = signal('0');
+  enteredExpectedReturn = signal('5');
+  enteredDuration = signal('10');
 
   onSubmit() {
-    console.log('Form submitted!');
-    console.log(this.enteredInitialInvestment);
-    console.log(this.enteredAnnualInterest);
-    console.log(this.enteredExpectedReturn);
-    console.log(this.enteredDuration);
+    this.calculate.emit({
+      initialInvestment: +this.enteredInitialInvestment(),
+      annualInvestment: +this.enteredAnnualInterest(),
+      expectedReturn: +this.enteredExpectedReturn(),
+      duration: +this.enteredDuration(),
+    });
+    this.enteredInitialInvestment.set('0');
+    this.enteredAnnualInterest.set('0');
+    this.enteredExpectedReturn.set('5');
+    this.enteredDuration.set('10');
   }
-
 }
