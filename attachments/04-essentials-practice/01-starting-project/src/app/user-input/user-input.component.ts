73b47154a04639e5c5ca710db8f70090ba44adc6
Carolina Bonitatis <carolina@bonitat.is>
2024-11-15 16:13:39 -0500

    add signals for cross component communication

--- attachments/04-essentials-practice/01-starting-project/src/app/user-input/user-input.component.ts
+++ attachments/04-essentials-practice/01-starting-project/src/app/user-input/user-input.component.ts
@@ -1,6 +1,7 @@
-import { Component, output, signal } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import type { InvestmentInput } from '../investment-input.model';
+
+import { InvestmentService } from '../investment.service';
 
 @Component({
   selector: 'app-user-input',
@@ -10,19 +11,21 @@ import type { InvestmentInput } from '../investment-input.model';
   styleUrl: './user-input.component.css',
 })
 export class UserInputComponent {
-  calculate = output<InvestmentInput>();
   enteredInitialInvestment = signal('0');
   enteredAnnualInterest = signal('0');
   enteredExpectedReturn = signal('5');
   enteredDuration = signal('10');
 
+  constructor(private investmentService: InvestmentService) {}
+
   onSubmit() {
-    this.calculate.emit({
+    this.investmentService.CalculateInvestmentResults({
       initialInvestment: +this.enteredInitialInvestment(),
       annualInvestment: +this.enteredAnnualInterest(),
       expectedReturn: +this.enteredExpectedReturn(),
       duration: +this.enteredDuration(),
     });
+
     this.enteredInitialInvestment.set('0');
     this.enteredAnnualInterest.set('0');
     this.enteredExpectedReturn.set('5');
