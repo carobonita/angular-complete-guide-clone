84a691e09b27510e335664675b8eb8e4a01c4cbe
Carolina Bonitatis <carolina@bonitat.is>
2025-09-16 20:40:33 -0400

    adding viewChild and ContentChild to new-ticket component

--- attachments/06-cmp-deep-dive/01-starting-project/src/app/shared/control/control.component.ts
+++ attachments/06-cmp-deep-dive/01-starting-project/src/app/shared/control/control.component.ts
@@ -1,4 +1,11 @@
-import { Component, input, ViewEncapsulation } from '@angular/core';
+import {
+  Component,
+  ContentChild,
+  ElementRef,
+  inject,
+  input,
+  ViewEncapsulation,
+} from '@angular/core';
 
 @Component({
   selector: 'app-control',
@@ -9,8 +16,19 @@ import { Component, input, ViewEncapsulation } from '@angular/core';
   encapsulation: ViewEncapsulation.None,
   host: {
     class: 'control',
+    '(click)': 'onClick()',
   },
 })
 export class ControlComponent {
   label = input.required<string>();
+  private el = inject(ElementRef);
+  @ContentChild('input') private control?: ElementRef<
+    HTMLInputElement | HTMLTextAreaElement
+  >;
+
+  onClick() {
+    console.log('clicked');
+    console.log(this.el);
+    console.log(this.control);
+  }
 }
